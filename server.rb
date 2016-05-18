0b050f8785adcc1a113b91aef7b4d003f6e55ad4
Kasama <ralegro@bol.com.br>
2016-05-18 17:23:20 -0300

    trying to get post data

--- server.rb
+++ server.rb
@@ -4,7 +4,32 @@ server = TCPServer.new 25566
 
 loop do
 	client = server.accept
-	while line = client.gets
-		puts line
+	header = client.recv(1024)
+	puts "got header:\n#{header.to_s}"
+	length = /Content-Length: (\d+)/.match.captures.first.to_i
+	puts "got len: #{length}"
+	data = client.recv(length.to_i)
+
+	puts data.inspect
+	puts data
+
+	client.close
+
+=begin
+	client = server.accept
+	line = ''
+	while thisline = client.gets
+		line << thisline
+		line << '\n'
 	end
+	length = /Content-Length: (\d+)/.match(line)
+	next unless length
+	length = length.captures
+	puts length.inspect
+	length = length.first.to_i
+
+	data = client.read(length)
+	puts data.inspect
+=end
+
 end
