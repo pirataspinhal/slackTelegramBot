9afc6b89d3284a286b47e3f3ec62b9d4787e2195
Kasama <ralegro@bol.com.br>
2016-05-18 15:32:56 -0300

    Testing with POST data

--- server.rb
+++ server.rb
@@ -4,7 +4,17 @@ server = TCPServer.new 25566
 
 loop do
 	client = server.accept
-	while line = client.gets
-		puts line
+	method, path = client.gets.split
+	headers = {}
+	while line = client.gets.split(' ', 2)
+		break if line[0] = ''
+		headers[line[0].chop] = line[1].strip
 	end
+	data = client.read(headers['Content-Length'].to_i)
+
+	puts "Method: #{method}\n path: #{path}"
+
+	puts data
+
+	puts headers.inspect
 end
